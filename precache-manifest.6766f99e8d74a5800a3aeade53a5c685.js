8daf5563d5ade1e67e90c253ba32680ba060d292
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2020-06-25 16:55:36 -0300

    Updates

---
+++ precache-manifest.6766f99e8d74a5800a3aeade53a5c685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdbcce3ccb4aaa0c5b0ef919c76f043",
+    "url": "/ManeMaria/react-spa-modelo-sfl/index.html"
+  },
+  {
+    "revision": "2599d0c5054762411f7e",
+    "url": "/ManeMaria/react-spa-modelo-sfl/static/css/main.55a7e2c8.chunk.css"
+  },
+  {
+    "revision": "fa7bcd32dcd5c1d68a46",
+    "url": "/ManeMaria/react-spa-modelo-sfl/static/js/2.2d50dabc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ManeMaria/react-spa-modelo-sfl/static/js/2.2d50dabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2599d0c5054762411f7e",
+    "url": "/ManeMaria/react-spa-modelo-sfl/static/js/main.8dfc0de5.chunk.js"
+  },
+  {
+    "revision": "bc3a31a083075ef53d5d",
+    "url": "/ManeMaria/react-spa-modelo-sfl/static/js/runtime-main.1a8024ec.js"
+  }
+]);
